390937728856f7db5ab390b480871879eca3ffa1
Jothika <123849832+Jothika-K-S@users.noreply.github.com>
2026-01-10 14:48:58 +0530

    Update and rename config.js to .env

--- backend/config.js
+++
@@ -1,3 +0,0 @@
-export const PORT = process.env.PORT;
-
-export const MONGO_URL = 'mongodb+srv://jothikakanthuluvaseenivasan:OFffSxzNgLOyHw2R@practice.ih5tixr.mongodb.net/books-collection?retryWrites=true&w=majority&appName=practice'
